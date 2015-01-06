9bfdf58edf4e2ea68681ac37566d1d82a7f8d8c9
patocox <patocox@users.noreply.github.com>
2015-01-06 11:41:24 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker build -t cakk/pmxdeploy .
 
 docker run --rm -it -e "REMOTE_TARGET_NAME=BBOX" -e CLUSTER_HOST=brightbox -e NODE_COUNT=2 -e CLIENT_ID=******* -e CLIENT_SECRET=******** -e VM_SIZE=nano cakk/pmxdeploy:latest
 #docker run --rm -it -e "REMOTE_TARGET_NAME=DOCEAN"  -e CLUSTER_HOST=digitalocean -e NODE_COUNT=2 -e DIGITALOCEAN_TOKEN=******* -e REGION=nyc3 -e "SSH_KEY_NAME=macbook air" -e VM_SIZE=512MB cakk/pmxdeploy:latest
-#docker run --rm -it -e "REMOTE_TARGET_NAME=AMAZON" -e CLUSTER_HOST=amazon -e NODE_COUNT=2 -e AWS_ACCESS_KEY_ID=AKIAJYG5IZ5QEEFA3KQQ -e "AWS_SECRET_ACCESS_KEY=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" -e REGION=us-east-1b -e "SSH_KEY_NAME=mac" -e VM_SIZE=t1.micro cakk/pmxdeploy:latest
+#docker run --rm -it -e "REMOTE_TARGET_NAME=AMAZON" -e CLUSTER_HOST=amazon -e NODE_COUNT=2 -e AWS_ACCESS_KEY_ID=****** -e "AWS_SECRET_ACCESS_KEY=*****" -e REGION=us-east-1b -e "SSH_KEY_NAME=mac" -e VM_SIZE=t1.micro cakk/pmxdeploy:latest
 #docker run --rm -it -e "REMOTE_TARGET_NAME=CENTURYLINK"   -e CLUSTER_HOST=centurylink -e NODE_COUNT=2  -e "API_KEY=********" -e "API_PASSWORD=********" -e REGION=UC1 -e VM_SIZE=nano -e "NETWORK_NAME=CoreOS Test DHCP Network" cakk/pmxdeploy:latest
